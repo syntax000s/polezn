6dea2078e52663f3f31c6686c1d532b33bcf2295
Дмитрий <112975910+syntax000s@users.noreply.github.com>
2023-10-26 16:16:37 +0700

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string f= "123456727391746279194520481";
+    cout<<"{";
+    for(int i=0,j=1;i<25;i++,j++)
+    {
+        cout<<f[i];
+        if(j==5)
+        {
+            j=0;
+            cout<<"}{";
+        }
+        else{cout<<',';}
+    }
+    cout<<"}";
+
+    return 0;
+}
+
